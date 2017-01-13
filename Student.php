e64cf66476d03a62ff64b9d71d4a30976aad48d1
Eva <eva.yw.yu@gmail.com>
2017-01-13 15:02:47 -0800

    EY: updated Student.php with additional support: to string function & average

--- Student.php
+++ Student.php
@@ -8,6 +8,8 @@
 **/
 
 class Student{
+    
+    //CTOR of the student class 
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -15,11 +17,31 @@ class Student{
         $this->grades = array();
     }
     
+    //Function will add a email to an array of emails of a student
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    //Function will add a grade to an array of grades of a student
     function add_grade($grade) {
         $this->grades[] = $grade;
+    }
+    
+    //Function will calculate the average of a student based on grades 
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    
+    //a basic to string operation that will show the details of the student 
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' ('.$this->average().")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': '. $what. "\n";
+        $result .= "\n";
+        return '<pre>'.$result.'</pre>';
     }
 }

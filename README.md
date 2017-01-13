47b63901f013105871c6bce9e618ac33fbf9c293
Eva <eva.yw.yu@gmail.com>
2017-01-13 14:44:59 -0800

    EY: First Commit -- Changed Readme, added index.php with basic hello

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # COMP4711Lab1
+==================
+
 BCIT COMP4711 Lab1
+So far, the read me is empty! 
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>COMP4711Lab1</title>
+</head>
+<body>
+    <?php
+        echo "Hello ... is there anybody out there?!"
+    ?>
+</body>
+</html>
